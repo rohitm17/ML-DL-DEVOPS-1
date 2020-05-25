1fe71e65325e9fa48a9402307979d6773f73806c
Rohit Mittal <35946128+rohitm17@users.noreply.github.com>
2020-05-25 22:27:34 +0530

    Create mail1.py

---
+++ mail1.py
@@ -0,0 +1,22 @@
+import smtplib
+a=" Welcome To E-mail Sender Tool "
+print(a.center(100,'*'))
+print()
+m="Please enable third-party access in your emails security settings.In case of gmail visit : https://myaccount.google.com/security  then enable less secure apps"
+print(m)
+print()
+
+sender_email=rohit.1705.mittal@gmail.com
+password=input(str("Enter your Password : "))
+rec_email=input(str("Enter recepient Email : "))
+message=input("Enter your Text-message : ")
+
+server=smtplib.SMTP('smtp.gmail.com',587)
+server.ehlo()
+server.starttls()
+
+server.login(sender_email,password)
+print("LogIn Successfull")
+
+server.sendmail(sender_email,rec_email,message)
+print("Email has been sent successfully to "+rec_email)
